6c539c29933c628365ecb9bb02d7b3e87ee43ce7
童小托 <tongziqi@gmail.com>
2017-01-22 14:01:11 +0800

    创建项目

---
+++ PM2.5/PM2.5/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  PM2.5
+//
+//  Created by 童小托 on 2017/1/22.
+//  Copyright © 2017年 童小托. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
