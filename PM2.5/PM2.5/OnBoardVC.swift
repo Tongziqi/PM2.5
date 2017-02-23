e275d2367e1bf40380f83bfd54d5493e5e7be271
童小托 <tongziqi@gmail.com>
2017-02-23 20:24:27 +0800

    修改了引导页

--- PM2.5/PM2.5/OnBoardVC.swift
+++ PM2.5/PM2.5/OnBoardVC.swift
@@ -13,19 +13,32 @@ extension AppDelegate {
         func generateStandardOnboardingVC() -> OnboardingViewController {
         var onboardVC = OnboardingViewController()
         // Create slides
-        let firstPage = OnboardingContentViewController.content(withTitle: "Welcome To pm2.5", body: "啦啦啦的pm2.5", image: UIImage(named: "onboard"), buttonText: nil, action: nil)
-        let secondPage = OnboardingContentViewController.content(withTitle: "Step 1", body: "第一页", image: UIImage(named: "onboard"), buttonText: nil, action: nil)
-        let thirdPage = OnboardingContentViewController.content(withTitle: "Step 2", body: "第二页", image: UIImage(named: "onboard"), buttonText: nil, action: nil)
-        let fourthPage = OnboardingContentViewController.content(withTitle: "Step 3", body: "第三页", image: UIImage(named: "onboard"), buttonText: "点击开始", action: self.skip)
-        // Define onboarding view controller properties
-        onboardVC = OnboardingViewController.onboard(withBackgroundImage: UIImage(named: "boardingImg"), contents: [firstPage, secondPage, thirdPage, fourthPage])
+        let firstPage = OnboardingContentViewController.content(withTitle: "Welcome To pm2.5", body: "苍穹之下，我们能做什么", image: UIImage(named: ""), buttonText: nil, action: nil)
+            firstPage.topPadding = 0
+            firstPage.underTitlePadding = 200
+        let secondPage = OnboardingContentViewController.content(withTitle: "Step 1", body: "第一页", image: UIImage(named: ""), buttonText: nil, action: nil)
+            secondPage.topPadding = 0
+            secondPage.underTitlePadding = 200
+        let thirdPage = OnboardingContentViewController.content(withTitle: "Step 2", body: "第二页", image: UIImage(named: ""), buttonText: nil, action: nil)
+            thirdPage.topPadding = 0
+            thirdPage.underTitlePadding = 200
+        let fourthPage = OnboardingContentViewController.content(withTitle: "Step 3", body: "第三页", image: UIImage(named: ""), buttonText: "点击开始", action: self.skip)
+            fourthPage.topPadding = 0
+            fourthPage.underTitlePadding = 200
+            fourthPage.actionButton.setTitleColor(UIColor.cyan, for: .normal)
+            // Define onboarding view controller properties
+        onboardVC = OnboardingViewController.onboard(withBackgroundImage: UIImage(named: "pm25"), contents: [firstPage, secondPage, thirdPage, fourthPage])
+        //淡入淡出效果
         onboardVC.shouldFadeTransitions = true
+        // 背景
         onboardVC.shouldMaskBackground = true
+        // 虚化背景
         onboardVC.shouldBlurBackground = false
         onboardVC.fadePageControlOnLastPage = true
-        onboardVC.pageControl.pageIndicatorTintColor = UIColor.darkGray
-        onboardVC.pageControl.currentPageIndicatorTintColor = UIColor.white
-        onboardVC.skipButton.setTitleColor(UIColor.black, for: .normal)
+            
+        onboardVC.skipButton.setTitle("Let's Go!", for: .normal)
+        onboardVC.skipButton.setTitleColor(UIColor.gray, for: .normal)
+            
         onboardVC.allowSkipping = true
         onboardVC.fadeSkipButtonOnLastPage = true
         onboardVC.skipHandler = {
