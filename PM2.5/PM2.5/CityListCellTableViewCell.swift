a438aa38e961b0f4929f1cd0ff139dbf6cd0efff
童小托 <tongziqi@gmail.com>
2017-03-01 14:10:17 +0800

    修改最低版本支持8.0，已知4s存在问题

--- PM2.5/PM2.5/CityListCellTableViewCell.swift
+++ PM2.5/PM2.5/CityListCellTableViewCell.swift
@@ -44,8 +44,20 @@ class CityListCellTableViewCell: UITableViewCell {
         locatedLabel.text = ""
         labelOfCity.text = ""
         selectionStyle = UITableViewCellSelectionStyle.none
-        contentView.layer.cornerRadius = 20
+        //contentView.layer.cornerRadius = 20
+//        contentView.layer.shadowColor = colorWithHex(0xCCCDCF).cgColor
+//        contentView.layer.shadowOffset = CGSize(width: 0, height: 5)
+//        contentView.layer.shadowOpacity = 2
+//        contentView.layer.shadowRadius = 4
+    }
+    
+    func colorWithHex(_ hex: Int, alpha: CGFloat = 1.0) -> UIColor {
+        let red = CGFloat(Float((hex >> 16) & 0xFF) / 255.0)
+        let green = CGFloat(Float((hex >> 8) & 0xFF) / 255.0)
+        let blue = CGFloat(Float((hex >> 0) & 0xFF) / 255.0)
+        let alpha = CGFloat(alpha)
         
+        return UIColor(red: red, green: green, blue: blue, alpha: alpha)
     }
     
     // 解决 ios8 tableview 自适应大小的时候，第一次展现的时候不生效的问题。
