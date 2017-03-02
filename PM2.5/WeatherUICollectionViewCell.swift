3dd6152c69acf795a029c1eb02510443342db32f
童小托 <tongziqi@gmail.com>
2017-03-02 15:50:20 +0800

    先加上IGListKit，再说

---
+++ PM2.5/WeatherUICollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  WeatherUICollectionViewCell.swift
+//  PM2.5
+//
+//  Created by 童小托 on 2017/3/2.
+//  Copyright © 2017年 童小托. All rights reserved.
+//
+
+import UIKit
+
+class WeatherUICollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
